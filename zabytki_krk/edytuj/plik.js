e97d09aa00df87f3f0542680c77e660db230cc13
Konrad Micek <49291148+miccu64@users.noreply.github.com>
2020-02-21 19:11:21 +0100

    Aktualka

--- zabytki_krk/edytuj/plik.js
+++ zabytki_krk/edytuj/plik.js
@@ -1,6 +1,11 @@
 function checkextension() {
-    var file = document.querySelector("#plik");
+    var file = document.querySelector("#recentPhotos");
     if (/\.(jpe?g|png|gif)$/i.test(file.files[0].name) === false) {
         alert("To nie jest zdjęcie lub niewłaściwy format!");
     }
 }
+
+$('.custom-file-input').on('change', function() {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+});
