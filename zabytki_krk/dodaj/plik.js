d1e1fdc7c25c9a265599c1d618f3fede9f3a076a
Konrad Micek <49291148+miccu64@users.noreply.github.com>
2020-02-26 19:06:26 +0100

    Delete plik.js

--- zabytki_krk/dodaj/plik.js
+++
@@ -1,6 +0,0 @@
-function checkextension() {
-    var file = document.querySelector("#recentPhotos");
-    if (/\.(jpe?g|png|gif)$/i.test(file.files[0].name) === false) {
-        alert("To nie jest zdjęcie lub niewłaściwy format!");
-    }
-}
