524ba36205aed2a51384c29698a59d8d2b17e8e4
Konrad M <98konrad98@gmail.com>
2020-07-25 19:15:45 +0200

    Dodanie ulubionych v1

--- szczegoly/skryptySzczeg.js
+++ szczegoly/skryptySzczeg.js
@@ -29,6 +29,27 @@ function dodajZdjecie() {
 
 wyslij();
 
+function ulubione() {
+    let params = new URLSearchParams(location.search);
+    var id = params.get('id');
+    var url2 = server + '/api/v1/artifacts/addToFavourites';
+    var token = localStorage.getItem('token');
+    console.log(id);
+    var http = new XMLHttpRequest();
+    http.open('POST', url2, true);
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.setRequestHeader('Authorization', token);
+    http.onreadystatechange = function() { //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            alert(http.response);
+        } else if (http.readyState == 4) {
+            alert('Błąd!');
+        }
+    }
+    http.send('id=' + id);
+}
+
 function wyslij() {
     let params = new URLSearchParams(location.search);
     var id = params.get('id');
